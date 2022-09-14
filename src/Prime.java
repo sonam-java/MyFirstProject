240ec0a4662a463c3736ae65dea36f19d0f84e2b
sonam-java <s06081994@gmail.com>
2022-09-13 17:00:56 -0700

    this is my first commit.

---
+++ src/Prime.java
@@ -0,0 +1,16 @@
+
+public class Prime {
+
+	public static void main(String[] args) {
+
+		int num=2;
+		
+		if(num%2==0) {
+			System.out.println("it is even number");
+		}
+		else {
+			System.out.println("It is not.");
+		}
+	}
+
+}
